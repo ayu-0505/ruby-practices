a7e630c9994a672a7d9dc0c3754132a5fd9a6651
ayu-0505 <amehuri.boshi.sh@icloud.com>
2024-03-01 10:57:02 +0900

    Shotクラスを作成

--- 06.bowling_object/lib/shot.rb
+++ 06.bowling_object/lib/shot.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class Shot
+  attr_reader :mark
+
+  def initialize(mark)
+    @mark = mark
+  end
+
+  def score
+    return 10 if mark == 'X'
+
+    mark.to_i
+  end
 end
--- 06.bowling_object/test/shot_test.rb
+++ 06.bowling_object/test/shot_test.rb
@@ -5,6 +5,16 @@ require_relative '../lib/shot'
 
 class ShotTest < Minitest::Test
   def test_shot
-    assert Shot.new
+    assert Shot.new(1)
+  end
+
+  def test_mark
+    shot = Shot.new(1)
+    assert_equal 1, shot.mark
+  end
+
+  def test_score
+    shot = Shot.new('X')
+    assert_equal 10, shot.score
   end
 end
