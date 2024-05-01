e3be895479eb9f290dc3796578ace4d3bbbf29ef
ayu-0505 <amehuri.boshi.sh@icloud.com>
2024-05-01 12:11:32 +0900

    count_file_datasメソッド追加、疑問点をコメントに追加

--- 07.ls_object/lib/list.rb
+++ 07.ls_object/lib/list.rb
@@ -11,7 +11,8 @@ attr_reader :file_datas
   end
 
   def file_name_width
-    @file_datas.map(&:filename).map(&:size).max
+    # @file_datas.map(&:filename).map(&:size).max　# mapと畳み込み演算では計算量的にどちらを選択するべきか
+    @file_datas.map(&:filename).reduce(0) {|result, path| [result, path.size].max}
   end
 
   def size_width
@@ -23,10 +24,16 @@ attr_reader :file_datas
   end
 
   def uid_name_width
-    @file_datas.map(&:user_name).map(&:size).max
+    #@file_datas.map(&:user_name).map(&:size).max
+    @file_datas.map(&:user_name).reduce(0) {|result, path| [result, path.size].max}
   end
 
   def gid_name_width
-    @file_datas.map(&:group_name).map(&:size).max
+    #@file_datas.map(&:group_name).map(&:size).max
+    @file_datas.map(&:group_name).reduce(0) {|result, path| [result, path.size].max}
+  end
+
+  def count_file_datas
+    @file_datas.count
   end
 end
