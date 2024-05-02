3e9eb4b39ea0250f9e7df25c734b51e6456aedec
ayu-0505 <amehuri.boshi.sh@icloud.com>
2024-05-02 11:41:42 +0900

    run_ls.rbファイル作成

---
+++ 07.ls_object/run_ls.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'optparse'
+require_relative 'lib/display'
+
+params = ARGV.getopts('arl')
+paths = params['a'] ?  Dir.glob('*', File::FNM_DOTMATCH ) : Dir.glob('*')
+paths.reverse! if params['r']
+display = Display.new(paths)
+puts (params['l'] ? display.render_long_list : display.render_short_list)
