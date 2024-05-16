96f6e2f4e1d4dec3c92c4ebbee4d0f2e0f11ca9a
ayu-0505 <amehuri.boshi.sh@icloud.com>
2024-05-16 14:54:31 +0900

    破壊的メソッドを非破壊的メソッドに変更

--- 07.ls_object/run_ls.rb
+++ 07.ls_object/run_ls.rb
@@ -6,6 +6,6 @@ require_relative 'lib/display'
 
 params = ARGV.getopts('arl')
 paths = params['a'] ? Dir.glob('*', File::FNM_DOTMATCH) : Dir.glob('*')
-paths.reverse! if params['r']
-display = Display.new(paths)
+sorted_paths = params['r'] ? paths.reverse : paths
+display = Display.new(sorted_paths)
 puts params['l'] ? display.render_long_list : display.render_short_list
